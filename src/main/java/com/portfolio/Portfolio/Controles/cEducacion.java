0c3f099b442ec05c7101ea2563f997331ac330b9
MateoNerli <mnerli2003@gmail.com>
2023-04-26 22:15:45 -0300

    correcciones

--- src/main/java/com/portfolio/Portfolio/Controles/cEducacion.java
+++ src/main/java/com/portfolio/Portfolio/Controles/cEducacion.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins =  {"https://portfolio-angular-df7c7.web.app/","http://localhost:4200"})
 
 public class cEducacion {
     @Autowired
